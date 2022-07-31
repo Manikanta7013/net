aa7a70d877ca49d0d4e184f51faae8d8a5084b79
Manikanta7013 <shanmukamanikanta6@gmail.com>
2022-08-01 01:58:23 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,26 @@
+
+import React, { useState, useEffect } from 'react';
+import "./Navbar.css";
+
+function Navbar() {
+    const [show, setShow] = useState(false)
+//   useEffect(() => {
+//     window.addEventListener("scroll", () => {
+//       if (window.scrollY > 100) {
+//         setShow(true);
+//       } else setShow(false);
+//     });
+//     return()=>{
+//         window.removeEventListener("scroll");
+//     }
+//   },[show]);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img className='nav__logo' src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt='Netflix Logo' />
+      <img className='nav__avatar' src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png' alt='User' />
+    </div>
+  );
+}
+
+export default Navbar;
